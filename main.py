f081a23f0a69c482bb0740b20607f4a26e88825a
virus gaming <79372051+franck403@users.noreply.github.com>
2022-09-06 22:39:26 +0000

    program are working :)

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 def check(text):
-    
+    if not (text.isalpha() or text.isdigit() or text.isspace()):
+        print("false")
+        
+    else:
+        print("true")
+
+check("") #put thee text or the var for the str input :)
